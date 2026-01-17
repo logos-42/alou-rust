ee7ac1af3db022036922e89f331a9b66ebf8e2c7
logos-42 <2844169590@qq.com>
2026-01-17 18:33:51 +0800

    feat: 更新桌面端版本至0.1.9

--- alou-edge/src/durable_objects/mod.rs
+++ alou-edge/src/durable_objects/mod.rs
@@ -5,6 +5,9 @@ pub mod ai_task_state;
 pub mod ai_task_execution;
 pub mod ai_task_ai;
 pub mod ai_task_workflow;
+pub mod ai_task_persistence;
+pub mod ai_task_alarm;
+pub mod ai_task_handlers;
 pub mod status_manager;
 pub mod task_executor;
 
